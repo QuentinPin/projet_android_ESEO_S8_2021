8bdeb4cc3660f4ff0931737eee8a1a7daa87b40e
Quentin <quentin1998-007@hotmail.fr>
2021-04-08 18:55:54 +0200

    Ajout de la main activity

--- app/src/main/java/com/example/projet_android_eseo_s8_2021/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/projet_android_eseo_s8_2021/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import com.example.projet_android_eseo_s8_2021.R
 import com.example.projet_android_eseo_s8_2021.databinding.ActivityMainBinding
 
