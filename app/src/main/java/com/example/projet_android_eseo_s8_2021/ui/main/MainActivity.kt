9b56a5deefccc3668674d2f77c121e1e8c2727fd
Quentin <quentin1998-007@hotmail.fr>
2021-04-08 20:03:14 +0200

    Ajout de toutes les animations sur la MainActivity

--- app/src/main/java/com/example/projet_android_eseo_s8_2021/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/projet_android_eseo_s8_2021/ui/main/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
+import android.view.animation.AnimationUtils
 import com.example.projet_android_eseo_s8_2021.R
 import com.example.projet_android_eseo_s8_2021.databinding.ActivityMainBinding
 import com.example.projet_android_eseo_s8_2021.ui.settings.SettingsActivity
@@ -23,8 +23,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Gestion des clicks bouttons
         binding.mainActivityImvSettings.setOnClickListener {
             startActivity(SettingsActivity.getStartedIntent(this))
         }
+
+        startAllAnimation()
+    }
+
+    private fun startAllAnimation() {
+        //On fait tourner la roue des settings ici
+        val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_animation)
+        binding.mainActivityImvSettings.animation = rotateAnimation
+
+        //On translate le logo ici
+        val translationXFromRightAnimation = AnimationUtils.loadAnimation(this, R.anim.translation_x_from_right_animation)
+        binding.mainActivityAppLogo.animation = translationXFromRightAnimation
+
+        //On translate le bouton 'location' ici
+        binding.mainActivityBtnLocation.animation = translationXFromRightAnimation
+
+        //On translate le bouton 'location' ici
+        val translationXFromLeftAnimation = AnimationUtils.loadAnimation(this, R.anim.translation_x_from_left_animation)
+        binding.mainActivityBtnHistory.animation = translationXFromLeftAnimation
     }
 }
--- app/src/main/java/com/example/projet_android_eseo_s8_2021/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/projet_android_eseo_s8_2021/ui/settings/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import com.example.projet_android_eseo_s8_2021.R
 import com.example.projet_android_eseo_s8_2021.databinding.ActivityMainBinding
 import com.example.projet_android_eseo_s8_2021.databinding.ActivitySettingsBinding
